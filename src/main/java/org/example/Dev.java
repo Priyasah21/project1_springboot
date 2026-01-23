2e1ff28450a3e273b73ea6a57640059f6dcca80f
Priya <priyanshi.sah890@gmail.com>
2026-01-23 15:02:13 +0530

    Initial springboot project

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev constructor");
+    }
+    public void build(){
+
+        System.out.println("working on awesome project");
+    }
+
+
+}
---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("laptop constructor");
+    }
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
